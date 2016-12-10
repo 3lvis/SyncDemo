e2b6101a3f51f98eef9558a9ca2f9450a6f7dd87
Elvis Nuñez <3lvis@users.noreply.github.com>
2016-12-10 02:22:38 +0100

    Final (#1)
    * Add frameworks
    
    * Simple insertion
    
    * Sync with Networking

--- Library/Fetchers/Fetcher.swift
+++ Library/Fetchers/Fetcher.swift
@@ -1,2 +1,35 @@
-class Fetcher {    
+import CoreData
+import Sync
+import Networking
+
+class Fetcher {
+    private let persistentContainer: NSPersistentContainer
+    private let networking: Networking
+
+    init() {
+        let modelName = "DataModel"
+        self.persistentContainer = NSPersistentContainer(name: modelName)
+        let containerURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+        let storeURL = containerURL.appendingPathComponent("\(modelName).sqlite")
+        try! self.persistentContainer.persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+
+        self.networking = Networking(baseURL: "https://jsonplaceholder.typicode.com")
+    }
+
+    func users(completion: @escaping (_ error: NSError?) -> ()) {
+        self.networking.GET("/users") { json, error in
+            if let error = error {
+                completion(error)
+            } else {
+                let usersJSON = json as! [[String: Any]]
+                Sync.changes(usersJSON, inEntityNamed: User.entity().name!, predicate: nil, persistentContainer: self.persistentContainer, operations: [.All], completion: completion)
+            }
+        }
+    }
+
+    func fetchLocalUsers() -> [User] {
+        let request: NSFetchRequest<User> = User.fetchRequest()
+
+        return try! self.persistentContainer.viewContext.fetch(request)
+    }
 }
---
+++ Podfile
@@ -0,0 +1,6 @@
+use_frameworks!
+
+target 'iOS' do
+  pod 'Sync', '~> 2.0'
+  pod 'Networking', '~> 2.0'
+end
